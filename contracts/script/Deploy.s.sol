0cf7ad8215b3ab6a0f8b4b224f6d0736ebde11ee
Raouf <raouf.hec@gmail.com>
2025-07-05 14:37:48 +0200

    created tests for deploying contracts

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -3,12 +3,31 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
 import {PixelStaking} from "../src/PixelStaking.sol";
+import {MintableToken} from "../src/MintableToken.sol";
 
 contract DeployPixel is Script {
+    MintableToken public token;
+
     function run() external {
         console.log("=== Starting Contract Deployment ===");
 
+        console.log("=== Starting Contract Deployment ===");
         vm.startBroadcast();
+
+        console.log("Deploying Token contract...");
+        token = new MintableToken("PXMT", "PIXAMUT");
+        console.log("Token deployed at:", address(token));
+
+        address deployer = msg.sender;
+        token.mint(deployer, 1_000_000 ether);
+        console.log("Minted 1,000,000 tokens to deployer");
+
+        // Deploy PixelStaking
+        console.log("Deploying PixelStaking contract...");
+        PixelStaking pixelStaking = new PixelStaking(deployer);
+        console.log("PixelStaking deployed at:", address(pixelStaking));
+
+        vm.stopBroadcast();
 
         // Deploy Token
         console.log("Deploying Token contract...");
