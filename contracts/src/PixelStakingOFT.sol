4fbf808b14a96012c6b37affdc31871d1cb92cb5
Raouf <raouf.hec@gmail.com>
2025-07-05 16:41:54 +0200

    starting with layerzero

---
+++ contracts/src/PixelStakingOFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Your base PixelStaking
+import {PixelStaking} from "./PixelStaking.sol";
+
+// LayerZero interfaces
+import {ILayerZeroEndpoint} from "@layerzerolabs/solidity-examples/contracts/interfaces/ILayerZeroEndpoint.sol";
+import {ILayerZeroReceiver} from "@layerzekrolabs/solidity-examples/contracts/interfaces/ILayerZeroReceiver.sol";
+
+contract PixelStakingOFT is PixelStaking, ILayerZeroReceiver {
+    /// @notice LayerZero endpoint for this chain
+    ILayerZeroEndpoint public immutable lzEndpoint;
+
+    /// @notice Mapping of remote chainId ⇒ trusted remote address bytes
+    mapping(uint16 => bytes) public trustedRemote;
+
+    /// @notice Replay protection for received messages
+    mapping(bytes32 => bool) public processedMessages;
+
+    /// @param _owner     the initial owner for Ownable
+    /// @param _endpoint  the LayerZero endpoint on this network
+    constructor(address _owner, address _endpoint) PixelStaking(_owner) {
+        lzEndpoint = ILayerZeroEndpoint(_endpoint);
+    }
+}
